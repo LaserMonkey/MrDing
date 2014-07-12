ccb6668fd771fc030ad2ed8f85b4ff0ffab43043
LaserMonkey <joeklaus@126.com>
2014-07-12 12:14:22 +0800

    测试移动位置

--- js/index.js
+++ js/index.js
@@ -1,23 +1,31 @@
 $(document).ready(function() {
-	console.log(($(document).height()-25-433)/2);
 	mainMediate();
 	$(window).resize(function(){
 		mainMediate();
 	});
 	questionMarkAnimate();
 	setInterval("questionMarkAnimate()", 1500);
+	showPlan();
 });
 
 function questionMarkAnimate() {
 	$("#img-mark").animate({
-		top:'25px'
+		top: "25px"
 	}, 400).animate({
-		top:'46px'
+		top: "46px"
 	}, 400);
 }
 
 function mainMediate() {
 	$("#main").css({
 		"padding-top": ($(document).height()-25-433)/2
+	});
+}
+
+function showPlan() {
+	$("#plan-button").click(function() {
+		$("#human").animate({
+			"min-width": "400px"
+		}, 400);
 	});
 }
